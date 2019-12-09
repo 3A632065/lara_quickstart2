4b4708dbd56938ff2197d1cc7c06721b03f32c60
xinru <s3a632065@student.ncut.edu.tw>
2019-12-10 00:09:50 +0800

    建置一些對應至此控制器的路由

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tasks', 'TaskController@index');
+
+Route::post('/task', 'TaskController@store');
+
+Route::delete('/task/{task}', 'TaskController@destroy');
+
+/*
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+*/
