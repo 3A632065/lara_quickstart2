afc47b553d74636148c08ea6727f376ca8ad8cbc
xinru <s3a632065@student.ncut.edu.tw>
2019-12-10 00:09:18 +0800

    在 User 與 Task 模型定義與 tasks 與 user 的關聯 並呼叫 Eloquent 提供的 hasMany 方法

--- app/Task.php
+++ app/Task.php
@@ -11,4 +11,12 @@ class Task extends Model
      * @var array
      */
     public $fillable = ['name'];
+
+    /*
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
