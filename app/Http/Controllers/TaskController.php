ae4c0b3ee17650fab288050121171bd1d3abcd3c
xinru <s3a632065@student.ncut.edu.tw>
2019-12-10 00:47:00 +0800

    認證所有的任務路由

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
